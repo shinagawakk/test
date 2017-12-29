5df1c3be7e647efe801afcc1121b2701bf0f584a
shinagawakk <34932037+shinagawakk@users.noreply.github.com>
2017-12-29 19:37:41 +0900

    a

---
+++ App1/App1/App1/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using App1.Views;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
